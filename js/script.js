83515de241769b6d1a98422e80c74f21547d8632
Egor <iegorka23@gmail.com>
2023-01-31 12:59:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// "use strict";
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console. log(number);
+
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// }
+
+// alert('Hello');
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log (typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамиия?', '');
+// answers[2] = prompt('Сколько лет?', '');
+
+// console.log(typeof(answers));
+
+const category = 'toys'
+console.log (`https://someurl.com/${category}/5`)
+const user = "Ivan"
+alert(`Привет,${user}`)
